bf6a31b8b3c3193a06e32cdbd02d47ea5023bc4f
bramulous <bramvalstar@gmail.com>
2016-12-07 19:46:24 +0100

    Zoomfunctions added. Still need styling and refactoring

--- app/js/aframefunctions.js
+++ app/js/aframefunctions.js
@@ -2,22 +2,50 @@
  * Created by Zowie on 12/6/2016.
  */
 $(document).ready(function () {
+//hoverboard stroke colors
+    var color_none = document.getElementById('color-none');
+    var color_blue = document.getElementById('color-blue');
+    var color_red = document.getElementById('color-red');
 
-var color_none = document.getElementById('color-none');
-var color_blue = document.getElementById('color-blue');
-var color_red = document.getElementById('color-red');
+//hoverboard stroke en hoverboard base
+    var $hoverboard_stroke = $('#hoverboard-stroke');
+    var $hoverboard_base = $('#hoverboard-base');
 
-var $hoverboard_stroke = $('#hoverboard-stroke');
+//zoombuttons
+    var zoomIn = document.getElementById('zoomInButton');
+    var zoomOut = document.getElementById('zoomOutButton');
 
-addStroke(color_none, $hoverboard_stroke, '#hoverboard-stroke-none-obj', '#hoverboard-stroke-none-obj');
-addStroke(color_blue, $hoverboard_stroke, '#hoverboard-stroke-blue-obj', '#hoverboard-stroke-blue-mtl');
-addStroke(color_red, $hoverboard_stroke, '#hoverboard-stroke-red-obj', '#hoverboard-stroke-red-mtl');
+//zoom variabelen aanmaken
+    var zoomvar = 1;
+    var zoommin = .1;
+    var zoomplus = -.1
+
+//aanroepen functie om kleur aan te passen
+    addStroke(color_none, $hoverboard_stroke, '#hoverboard-stroke-none-obj', '#hoverboard-stroke-none-obj');
+    addStroke(color_blue, $hoverboard_stroke, '#hoverboard-stroke-blue-obj', '#hoverboard-stroke-blue-mtl');
+    addStroke(color_red, $hoverboard_stroke, '#hoverboard-stroke-red-obj', '#hoverboard-stroke-red-mtl');
 
 // functie voor texture toevoegen op mouseenter
-function addStroke(id, selector, obj, mtl ) {
-    id.addEventListener('mouseenter', function () {
-        selector.attr('obj-model', 'obj :' + obj + '; mtl: ' + mtl + '');
-    });
-}
+    function addStroke(id, selector, obj, mtl) {
+        id.addEventListener('mouseenter', function () {
+            selector.attr('obj-model', 'obj :' + obj + '; mtl: ' + mtl + '');
+        });
+    }
+
+//aanroepen zoomfunctie
+    zoom(zoomIn, $hoverboard_stroke, $hoverboard_base, zoomvar, zoommin);
+    zoom(zoomOut, $hoverboard_stroke, $hoverboard_base, zoomvar, zoomplus);
+
+//zoom functie aanmaken
+
+    function zoom(id, stroke, base, zoomvar, zoomAdd) {
+        id.addEventListener('mouseenter', function () {
+            zoomvar = zoomvar + zoomAdd;
+            stroke.attr('scale', zoomvar, zoomvar, zoomvar);
+            base.attr('scale', zoomvar, zoomvar, zoomvar);
+
+        });
+    }
 
 });
+
