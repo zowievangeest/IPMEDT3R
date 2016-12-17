6903921bd33e6bdc026b9318ea0e5232d6888a74
Zowie van Geest <zowie@zowievangeest.nl>
2016-12-17 11:19:53 +0100

    rewrited javscript for OOP

---
+++ app/js/aframe_functions.js
@@ -0,0 +1,101 @@
+/**
+ * Created by Zowie on 12/17/2016.
+ */
+var zoomvar = 1;
+
+var hoverboardManager = {
+    init: function () {
+        hoverboardManager.setListeners();
+    },
+
+    setListeners: function () {
+        hoverboardManager.functions.main_functions();
+    },
+
+    functions: {
+        main_functions: function () {
+            //hoverboard stroke colors
+            var color_none = document.getElementById('color-none');
+            var color_blue = document.getElementById('color-blue');
+            var color_red = document.getElementById('color-red');
+
+            //hoverboard stroke en hoverboard base
+            var $hoverboard_stroke = $('#hoverboard-stroke');
+            var $hoverboard_base = $('#hoverboard-base');
+
+            //variabelen voor rotate buttons
+            var rotate_right = document.getElementById('rotateRight');
+            var rotate_left = document.getElementById('rotateLeft');
+            var $rotAnBoard = $('#rotateAnimationBoard');
+            var $rotAnStroke = $('#rotateAnimationStroke');
+
+            //zoombuttons
+            var zoomIn = document.getElementById('zoomInButton');
+            var zoomOut = document.getElementById('zoomOutButton');
+
+            //zoom variabelen aanmaken
+            var zoommin = .1;
+            var zoomplus = -.1;
+
+            hoverboardManager.functions.addStroke(color_none, $hoverboard_stroke, '#hoverboard-stroke-none-obj', '#hoverboard-stroke-none-obj');
+            hoverboardManager.functions.addStroke(color_blue, $hoverboard_stroke, '#hoverboard-stroke-blue-obj', '#hoverboard-stroke-blue-mtl');
+            hoverboardManager.functions.addStroke(color_red, $hoverboard_stroke, '#hoverboard-stroke-red-obj', '#hoverboard-stroke-red-mtl');
+
+            //aanroepen zoomfunctie
+            hoverboardManager.functions.zoom(zoomIn, $hoverboard_stroke, $hoverboard_base, zoommin);
+            hoverboardManager.functions.zoom(zoomOut, $hoverboard_stroke, $hoverboard_base, zoomplus);
+
+            // aanroepen rotation functies
+            hoverboardManager.functions.rotationRight(rotate_right, $rotAnBoard, $rotAnStroke);
+            hoverboardManager.functions.rotationLeft(rotate_left, $rotAnBoard, $rotAnStroke);
+
+            for (i = 1; i < 9; i++) {
+                var color = document.getElementById("skybox-color" + i);
+                color.addEventListener('mouseenter', function () {
+                    var colorCode = $(this).attr('material')['color'];
+                    $('#sky').attr('color', colorCode);
+                })
+            }
+        },
+
+        addStroke: function (id, selector, obj, mtl) {
+            id.addEventListener('mouseenter', function () {
+                selector.attr('obj-model', 'obj :' + obj + '; mtl: ' + mtl);
+            });
+        },
+
+        zoom: function (id, stroke, base_hov, zoomAdd) {
+            id.addEventListener('mouseenter', function () {
+                hoverboardManager.functions.controlZoom(zoomAdd, stroke, base_hov);
+            });
+        },
+
+        controlZoom: function (zoomAdd, stroke, base_hov) {
+            zoomvar += zoomAdd;
+            stroke.attr('scale', zoomvar, zoomvar, zoomvar);
+            base_hov.attr('scale', zoomvar, zoomvar, zoomvar);
+            console.log(zoomvar)
+        },
+
+        rotationRight: function (id, strokeAn, boardAn) {
+            id.addEventListener('mouseenter', function(){
+                console.log("hovered");
+                strokeAn.attr('repeat', 0).attr('dur', 5000);
+                boardAn.attr('repeat', 0).attr('dur', 5000);
+            });
+        },
+
+        rotationLeft: function (id, strokeAn, boardAn) {
+            id.addEventListener('mouseenter', function(){
+                console.log("hovered");
+                strokeAn.attr('repeat', '0').attr('dur', 5000).attr('direction', 'reverse');
+                boardAn.attr('repeat', '0').attr('dur', 5000).attr('direction', 'reverse');
+            });
+        }
+
+    }
+};
+
+$(document).ready(function () {
+    hoverboardManager.init();
+});
